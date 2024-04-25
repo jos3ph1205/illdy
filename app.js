d39da5416b03c12bb607b47e99eeeb066279bc25
Joseph5 <153535704+jos3ph1205@users.noreply.github.com>
2024-04-25 22:37:44 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -7,20 +7,32 @@ const mainSectionObserver = new IntersectionObserver(
    (entries) => {
       entries.forEach((entry) => {
          let secId = entry.target.getAttribute("id");
-         console.log(entry, secId)
+         console.log(entry, secId);
 
          if (entry.isIntersecting) {
             navLinks.forEach((link) => {
                link.classList.remove("active");
             });
 
-            document.querySelector(`.nav-links a[href*="${secId}"]`).classList.add("active");
+            document.querySelectorAll(`.nav-links a[href*="${secId}"]`).forEach((el) => {
+               el.classList.add("active");
+            })
          }
       });
    },
    {
-      threshold: .7,
+      threshold: 0.7,
    }
 );
 
 mainSection.forEach((el) => mainSectionObserver.observe(el));
+
+let menuToggleBtn = document.querySelectorAll(".mob-menu-tog");
+let mobileMenu = document.querySelector(".mobile-menu");
+
+menuToggleBtn.forEach((btn) => {
+   btn.addEventListener("click", () => {
+      btn.classList.toggle("show");
+      mobileMenu.classList.toggle("show");
+   });
+});
